0f404e48d48a80849111055c03a865d1650f9415
adamhinckley <adamhinckley@mac.com>
2019-04-04 14:50:33 -0600

    cleanup

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -5,7 +5,6 @@
 
 #define DATA_LEN 6
 
-//We'll make use of these helper function later
 unsigned char cpu_ram_read(struct cpu *cpu, unsigned char address)
 {
   return cpu->ram[address];
@@ -21,16 +20,6 @@ void cpu_ram_write(struct cpu *cpu, unsigned char address, unsigned char value)
  */
 void cpu_load(struct cpu *cpu, char *file)
 {
-  // char data[DATA_LEN] = {
-  //     // From print8.ls8
-  //     0b10000010, // LDI R0,8
-  //     0b00000000,
-  //     0b00001000,
-  //     0b01000111, // PRN R0
-  //     0b00000000,
-  //     0b00000001 // HLT
-  // };
-
   FILE *fp;
   char line[1024];
 
@@ -56,13 +45,6 @@ void cpu_load(struct cpu *cpu, char *file)
     }
     cpu->ram[address++] = value;
   }
-
-  // for (int i = 0; i < DATA_LEN; i++)
-  // {
-  //   cpu->ram[address++] = data[i];
-  // }
-
-  // TODO: Replace this with something less hard-coded
 }
 
 /**
@@ -127,11 +109,7 @@ void cpu_run(struct cpu *cpu)
 
   while (running)
   {
-    // TODO
-    // 1. Get the value of the current instruction (in address PC).
     unsigned char IR = cpu_ram_read(cpu, cpu->PC);
-    // 2. Figure out how many operands this next instruction requires
-    // 3. Get the appropriate value(s) of the operands following this instruction
     unsigned char operandA;
     unsigned char operandB;
     int next_line = 1;
@@ -148,7 +126,6 @@ void cpu_run(struct cpu *cpu)
       next_line = 2;
     }
 
-    // 4. switch() over it to decide on a course of action.
     switch (IR)
     {
     case ADD:
@@ -244,7 +221,6 @@ void cpu_run(struct cpu *cpu)
  */
 void cpu_init(struct cpu *cpu)
 {
-  // TODO: Initialize the PC and other special registers
   cpu->PC = 0;
   cpu->FL = 0;
   cpu->E = 0;
